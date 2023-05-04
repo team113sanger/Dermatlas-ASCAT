d927c38ccc763e6f4429f23a832a805eba858bad
Keiran Raine <keiran.raine@eahsn.org>
2023-05-04 12:42:58 +0000

    Changes to hf init

--- .devcontainer/extra.sh
+++ .devcontainer/extra.sh
@@ -5,3 +5,7 @@ set -o pipefail
 
 (cd /tmp && sudo rm -rf gitflow && git clone https://github.com/datasift/gitflow && cd gitflow && sudo ./install.sh && sudo git hf upgrade)
 sudo rm -rf /tmp/gitflow
+
+set +e
+# this fails if unstaged changes, however they should only exist when resuming
+git hf init -f
